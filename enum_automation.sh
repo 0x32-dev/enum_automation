a28e62f7c529899cb3905e91a7bb143b0996962a
0x32-dev <liamsteam@gmail.com>
2025-04-04 07:03:27 -0400

    Added Subdomain Enum + Nmap UDP

--- enum_automation.sh
+++ enum_automation.sh
@@ -11,8 +11,27 @@ fi
 host=$1
 ip=$2
 
+echo -e "\033[1;32m"
+echo "###############################################"
+echo "          Welcome to enum_automation           "
+echo "###############################################"
+echo "     Automating enumeration and discovery...   "
+echo "###############################################"
+echo -e "\033[0m"
+
+
 # Run the nmap scan against the provided host
-echo "Running sC, sV, -O, -p- $host...Outputting to file 'nmap.all'"
+echo "[+] Running nmap against all ports"
 nmap -sC -sV -p- -O "$host" | tee nmap.all
 
+# Run subdomain enumeration
+echo "[+] Running top 5000 domains"
+wfuzz --hc 404,301,400 -H "Host: FUZZ.$host" -w /usr/share/wordlists/amass/subdomains-top1mil-5000.txt $ip
 
+# Run nmap UDP Scan
+echo "[+] Runnning UDP Scan, this may take a while..."
+sudo nmap -sU $host | tee nmap.udp 
+
+# Running nmap UDP Scan on all ports
+echo "[+] Running UDP Scan on all ports, this will take even longer"
+sudo nmap -sU -p- $host | tee nmap.udp.all

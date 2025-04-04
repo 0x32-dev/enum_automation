8a0c722ecb8ccc186e432d923f6874ee5ce4587b
0x32-dev <liamsteam@gmail.com>
2025-04-04 06:38:08 -0400

    Updating Latest Script

--- enum_automation.sh
+++ enum_automation.sh
@@ -2,21 +2,17 @@
 
 # Check if a host is provided as an argument
 if [ -z "$1" ]; then
-  echo "Usage: $0 <host>"
-  echo "Example: $0 192.168.1.1"
+  echo "Use as ./enum_automation <dns> <ip>"
   exit 1
 fi
 
+
 # Assign the argument to a variable
 host=$1
+ip=$2
 
 # Run the nmap scan against the provided host
 echo "Running sC, sV, -O, -p- $host...Outputting to file 'nmap.all'"
 nmap -sC -sV -p- -O "$host" | tee nmap.all
 
-# Check if nmap command succeeded
-if [ $? -eq 0 ]; then
-  echo "nmap scan completed successfully."
-else
-  echo "nmap scan failed."
-fi
+
